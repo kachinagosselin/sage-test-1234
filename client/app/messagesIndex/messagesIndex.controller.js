1a02dbc1420358128db0807efdc7bbfbe09d93cb
Kachina Gosselin <kachina@alum.mit.edu>
2016-12-07 15:05:17 -0800

    remove duplicate message error

--- client/app/messagesIndex/messagesIndex.controller.js
+++ client/app/messagesIndex/messagesIndex.controller.js
@@ -1,8 +1,12 @@
 'use strict';
 
 angular.module('paizaqaApp')
-.controller('MessagesIndexCtrl', function ($scope) {
+.controller('MessagesIndexCtrl', function ($scope, $http) {
   $scope.busy = true;
   $scope.noMoreData = false;
+
+	$http.get('/api/messages').success(function(messages) {
+		console.log("Call once ... ")
+	});
 
 });
